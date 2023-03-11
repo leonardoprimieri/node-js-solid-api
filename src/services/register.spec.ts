7e5f3ec535b37da6af33ec22a3f3e8814509a3f0
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-03-11 18:08:54 -0300

    refactor: make test cleaner

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,36 +1,51 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { RegisterService } from './register.service'
 
 describe('Register Service', () => {
+  const mockedUser = {
+    email: 'jhon@doe.com',
+    name: 'Jhon Doe',
+    password: '123456',
+  }
+
+  const makeSut = () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(inMemoryUsersRepository)
+
+    return registerService
+  }
+
+  it('should be able to register', async () => {
+    const registerService = makeSut()
+
+    const { user } = await registerService.execute(mockedUser)
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password', async () => {
-    const registerService = new RegisterService({
-      async findByEmail() {
-        return null
-      },
+    const registerService = makeSut()
 
-      async create(data) {
-        return {
-          id: 'user-id',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
-
-    const { user } = await registerService.execute({
-      name: 'John Doe',
-      email: 'john@doe.com',
-      password: '123456',
-    })
+    const { user } = await registerService.execute(mockedUser)
 
     const isPasswordCorrectlyHashed = await compare(
-      '123456',
+      mockedUser.password,
       user.password_hash,
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to create an user that already exists', async () => {
+    const registerService = makeSut()
+
+    await registerService.execute(mockedUser)
+
+    expect(
+      async () => await registerService.execute(mockedUser),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
