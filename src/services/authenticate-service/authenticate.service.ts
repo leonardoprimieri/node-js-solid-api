1397a01108cb184caed4e555effee0f321ec0d3c
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-03-11 19:17:56 -0300

    feat: create authenticate service

--- src/services/authenticate-service/authenticate.service.ts
+++ src/services/authenticate-service/authenticate.service.ts
@@ -1,11 +1,16 @@
 import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
 
 type AuthenticateServiceParams = {
   email: string
   password: string
 }
 
-type AuthenticateServiceRequest = void
+type AuthenticateServiceRequest = {
+  user: User
+}
 
 export class AuthenticateService {
   constructor(private usersRepository: UsersRepository) {}
@@ -13,5 +18,19 @@ export class AuthenticateService {
   async execute({
     email,
     password,
-  }: AuthenticateServiceParams): Promise<AuthenticateServiceRequest> {}
+  }: AuthenticateServiceParams): Promise<AuthenticateServiceRequest> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
 }
