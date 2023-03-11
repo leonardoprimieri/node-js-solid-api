578c58948c47d1c82fbeb2214defacbf1649de6b
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-03-11 19:34:18 -0300

    feat: create route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticateController } from './controllers/authenticate/authenticate.controller'
 import { registerUserController } from './controllers/register-user/register-user.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerUserController)
+  app.post('/sessions ', authenticateController)
 }
